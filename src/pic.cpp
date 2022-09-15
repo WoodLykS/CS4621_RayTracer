34ba9e8b480c6f72e4fbb3a82dfe37b01c13eb48
Taylor Jiang <sj649@cornell.edu>
2022-09-15 14:45:11 -0400

    There is a difference in c++ 12 and c++ 13. The setup code does not work in c++13. So I edited only a few lines.

--- src/pic.cpp
+++ src/pic.cpp
@@ -19,9 +19,9 @@ int main()
     // std::cerr << "\rScanlines remaining: " << j << ' ' << flush;
     for (int i = 0; i < image_width; ++i)
     {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
+      double r = double(i) / (image_width - 1);
+      double g = double(j) / (image_height - 1);
+      double b = 0.25;
 
       int ir = static_cast<int>(255.999 * r);
       int ig = static_cast<int>(255.999 * g);
