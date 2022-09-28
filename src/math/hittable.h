5ec2cd0e84da8421d72bce16d158163d0026e8ef
sj649 <sj649@cornell.edu>
2022-09-28 01:52:30 -0400

    hittable, hittable_list, sphere, etc.

---
+++ src/math/hittable.h
@@ -0,0 +1,28 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+struct hit_record
+{
+  point3 p;
+  // normal unit vector to the surface;
+  // we assume it points "out"
+  vec3 normal;
+  double t;
+  // if the ray and the normal face in the same direction,
+  // the ray is inside the object; otherwise, outside
+  bool front_face;
+
+  inline void set_face_normal(ray r, vec3 outward_normal)
+  {
+    front_face = dot(r.velocity, outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
+};
+
+class hittable
+{
+public:
+  virtual bool hit(ray r, double t_min, double t_max, hit_record &rec);
+};
+#endif
--- src/picture.cpp
+++ src/picture.cpp
@@ -1,35 +1,99 @@
+//////////// Wed Sep 28 1:13 AM ////////////
+#include "math/util.h"
+#include "math/hittablelist.h"
+#include "math/sphere.h"
 #include <iostream>
-#include "math/vec3.h"
+
 using namespace std;
+
+// vertical linear color interpolation aka lerp
+color ray_color(ray r, hittable world)
+{
+  hit_record rec;
+  if (world.hit(r, 0, infinity, rec))
+  {
+    return (rec.normal + color(1, 1, 1)) * 0.5;
+  }
+  vec3 unit_velocity = r.velocity.normalize();
+  double t = 0.5 * (unit_velocity.y() + 1.0);
+  return color(1.0, 1.0, 1.0) * (1.0 - t) + color(0.5, 0.7, 1.0) * t;
+}
+
 int main()
 {
-  point3 a = vec3(1, 2, 3);
-  cout << a << endl;
+  // Image
+  const double aspect_ratio = 16.0 / 9.0;
+  const int image_width = 400;
+  const int image_height = static_cast<int>(image_width / aspect_ratio);
 
-  // freopen("1.ppm", "w", stdout);
-  //  Image
-  const int image_width = 2;
-  const int image_height = 2;
+  // World
+  hittable_list world;
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+  // Camera
+  double viewpoint_height = 2.0;
+  double viewpoint_width = aspect_ratio * viewpoint_height;
+  double focal_length = 1.0;
+
+  point3 origin = point3(0, 0, 0);
+  vec3 horizontal = vec3(viewpoint_width, 0, 0);
+  vec3 vertical = vec3(0, viewpoint_height, 0);
+  point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
   // Render
-  cout << "P3 \n"
-       << image_width << " " << image_height << " " << '\n';
-  cout << "255 " << '\n';
+  cout << "P3\n"
+       << image_width << ' ' << image_height << "\n255\n";
+
   for (int j = image_height - 1; j >= 0; --j)
   {
-    // std::cerr << "\rScanlines remaining: " << j << ' ' << flush;
+    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i)
     {
-      double r = double(i) / (image_width - 1);
-      double g = double(j) / (image_height - 1);
-      double b = 0.25;
-
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
-
-      cout << ir << ' ' << ig << ' ' << ib << ' ' << '\n';
+      double u = double(i) / (image_width - 1);
+      double v = double(j) / (image_height - 1);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      color pixel_color = ray_color(r, world);
+      write_color(std::cout, pixel_color);
     }
   }
-  // std::cerr << "\nDone.\n";
+
+  std::cerr << "\nDone.\n";
   return 0;
 }
+
+// #include <iostream>
+// #include "math/vec3.h"
+// using namespace std;
+// int main()
+// {
+//   point3 a = vec3(1, 2, 3);
+//   cout << a << endl;
+
+//   // freopen("1.ppm", "w", stdout);
+//   //  Image
+//   const int image_width = 2;
+//   const int image_height = 2;
+//   // Render
+//   cout << "P3 \n"
+//        << image_width << " " << image_height << " " << '\n';
+//   cout << "255 " << '\n';
+//   for (int j = image_height - 1; j >= 0; --j)
+//   {
+//     // std::cerr << "\rScanlines remaining: " << j << ' ' << flush;
+//     for (int i = 0; i < image_width; ++i)
+//     {
+//       double r = double(i) / (image_width - 1);
+//       double g = double(j) / (image_height - 1);
+//       double b = 0.25;
+
+//       int ir = static_cast<int>(255.999 * r);
+//       int ig = static_cast<int>(255.999 * g);
+//       int ib = static_cast<int>(255.999 * b);
+
+//       cout << ir << ' ' << ig << ' ' << ib << ' ' << '\n';
+//     }
+//   }
+//   // std::cerr << "\nDone.\n";
+//   return 0;
+// }
